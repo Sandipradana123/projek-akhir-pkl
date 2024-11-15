e4fcdc79e8d11d384357929d3f7110ec7df6dd6b
Sandipradana123 <sandipradana295@gmail.com>
2024-11-15 08:25:13 +0700

    penambahan file database

---
+++ dbMysql/jadwalukk.sql
@@ -0,0 +1,11 @@
+<div class="alert alert-danger" role="alert"><h1>Galat</h1><p><strong>Query SQL:</strong>  <a href="#" class="copyQueryBtn" data-text="SET time_zone = &quot;+00:00&quot;">Salin</a>
+<a href="index.php?route=/database/sql&sql_query=SET+time_zone+%3D+%22%2B00%3A00%22&show_query=1&db=jadwalukk"><span class="text-nowrap"><img src="themes/dot.gif" title="Ubah" alt="Ubah" class="icon ic_b_edit">&nbsp;Ubah</span></a>    </p>
+<p>
+<code class="sql"><pre>
+SET time_zone = &quot;+00:00&quot;
+</pre></code>
+</p>
+<p>
+    <strong>MySQL menyatakan: </strong><a href="./url.php?url=https%3A%2F%2Fdev.mysql.com%2Fdoc%2Frefman%2F8.0%2Fen%2Fserver-error-reference.html" target="mysql_doc"><img src="themes/dot.gif" title="Dokumentasi" alt="Dokumentasi" class="icon ic_b_help"></a>
+</p>
+<code>#2006 - MySQL server has gone away</code><br></div>
